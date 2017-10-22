6b00ce7a975f1ed294b6b4975e8f9a336b1020d8
Hepsilion <2428091608@qq.com>
2017-10-22 14:21:45 +0800

    Complete Perceptrons and LogisticRegression.

---
+++ src/main/java/org/dl/util/RandomGenerator.java
@@ -0,0 +1,9 @@
+package org.dl.util;
+
+import java.util.Random;
+
+public final class RandomGenerator {
+    public static double uniform(double min, double max, Random rng) {
+        return rng.nextDouble() * (max - min) + min;
+    }
+}
